0e9d0d7e28590dafa6e042e17a5ced90feca4c4a
Andreas Würl <awuerl@gmx.net>
2011-12-03 12:45:53 +0100

    added data view

--- trackerweb/__init__.py
+++ trackerweb/__init__.py
@@ -3,8 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 from trackerweb.views.tracker import tracker
+from trackerweb.views.data import data
 
 app.register_blueprint(tracker)
+app.register_blueprint(data)
 
 @app.route('/')
 def index():
