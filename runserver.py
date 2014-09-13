927ffffc36f1a5ed437cc35528798debc9391289
Andreas Würl <andi@tryb.de>
2014-09-13 22:32:15 +0200

    enable memcached cache if available

--- runserver.py
+++ runserver.py
@@ -3,9 +3,13 @@
 activate_this = 'bowebenv/bin/activate_this.py'
 execfile(activate_this, dict(__file__=activate_this))
 
-from werkzeug.contrib.cache import MemcachedCache
 from boweb import app
 
-app.config['cache'] = MemcachedCache(['127.0.0.1:11211'])
+try:
+    from werkzeug.contrib.cache import MemcachedCache
+
+    app.config['cache'] = MemcachedCache(['127.0.0.1:11211'])
+except ImportError:
+    pass
 
 app.run(host='0.0.0.0', debug=True)
