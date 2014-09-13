fed311e5f6219d9a7dda6cfec57193662827741c
Andreas Würl <andi@tryb.de>
2014-09-13 22:17:59 +0200

    reenabled memcache

--- runserver.py
+++ runserver.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-#activate_this = 'bowebenv/bin/activate_this.py'
-#execfile(activate_this, dict(__file__ = activate_this))
+activate_this = 'bowebenv/bin/activate_this.py'
+execfile(activate_this, dict(__file__=activate_this))
 
-#from werkzeug.contrib.cache import MemcachedCache
+from werkzeug.contrib.cache import MemcachedCache
 from boweb import app
 
-#app.config['cache'] = MemcachedCache(['127.0.0.1:11211'])
+app.config['cache'] = MemcachedCache(['127.0.0.1:11211'])
 
 app.run(host='0.0.0.0', debug=True)
