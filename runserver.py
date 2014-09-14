0325c6fa4858e94a384cceff291a7e3994757392
Andreas Würl <andi@tryb.de>
2014-09-14 11:27:22 +0200

    bootstrap style tracker info

--- runserver.py
+++ runserver.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
-activate_this = 'bowebenv/bin/activate_this.py'
-execfile(activate_this, dict(__file__=activate_this))
+import os
+
+if os.path.exists('bowebenv'):
+    activate_this = 'bowebenv/bin/activate_this.py'
+    execfile(activate_this, dict(__file__=activate_this))
 
 from boweb import app
 
@@ -9,7 +12,7 @@ try:
     from werkzeug.contrib.cache import MemcachedCache
 
     app.config['cache'] = MemcachedCache(['127.0.0.1:11211'])
-except ImportError:
+except RuntimeError, ImportError:
     pass
 
 app.run(host='0.0.0.0', debug=True)
